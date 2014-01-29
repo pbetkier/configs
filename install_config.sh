e5f3fd3159cf3f8d07ca22da20174934baf910d3
Piotr Betkier <pbetkier@gmail.com>
2014-01-29 20:13:34 +0100

    Removed bash config, use fish instead.

--- install_config.sh
+++ install_config.sh
@@ -9,15 +9,7 @@ echo "Installing config ..."
 echo " Creating backup ..."
 BACKUP_DIR=`date +"%d-%m-%Y_%s"`
 mkdir -p $CONFIGS_DIR/backups/$BACKUP_DIR
-`cp -RLt $CONFIGS_DIR/backups/$BACKUP_DIR $HOME/.{vim,vimrc,gitconfig,gitignore_global,git-completion.bash,bash,config/fish/config.fish}`
-
-echo " Appending source entry to .bashrc ..."
-SOURCE_CMD="source $CONFIGS_DIR/bash/additional_conf"
-
-#add entry if it doesn't exist yet
-if ! grep -Fxq "$SOURCE_CMD" $HOME/.bashrc; then
-    echo -e "#more conf added by configs apply.sh\n$SOURCE_CMD" >> $HOME/.bashrc
-fi
+`cp -RLt $CONFIGS_DIR/backups/$BACKUP_DIR $HOME/.{vim,vimrc,gitconfig,gitignore_global,config/fish/config.fish}`
 
 echo " Creating symlinks ..."
 `ln -sTf $CONFIGS_DIR/vim/vim $HOME/.vim`
@@ -26,8 +18,6 @@ echo " Creating symlinks ..."
 `ln -sTf $CONFIGS_DIR/git/gitconfig $HOME/.gitconfig`
 `ln -sTf $CONFIGS_DIR/git/gitignore_global $HOME/.gitignore_global`
 `ln -sTf $CONFIGS_DIR/git/git-completion.bash $HOME/.git-completion.bash`
-
-`ln -sTf $CONFIGS_DIR/bash/bash $HOME/.bash`
 
 `ln -sTf $CONFIGS_DIR/fish/config.fish $HOME/.config/fish/config.fish`
 `ln -sft $HOME/.config/fish/functions $CONFIGS_DIR/fish/functions/*`
